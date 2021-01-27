9b9c87ea6b12a81bef707635852f5b8f19673202
guyiyun1 <yiyun.gu@mail.utoronto.ca>
2021-01-27 13:36:48 +0800

    A2/Snakes and Ladders Markov (Reward) Process

--- Assignment2/snakesandladders.py
+++ Assignment2/snakesandladders.py
@@ -6,15 +6,18 @@ import matplotlib.pyplot as plt
 
 @dataclass(frozen=True)
 class PlayerState:
+    """
+    Player state
+    """
     position: int
-
-    def player_position(self) -> int:
-        return self.position
 
 
 class SnakesAndLaddersMPFinite(FiniteMarkovProcess[PlayerState]):
-
-    def __init__(self, snake_positions, snake_to_positions, ladder_positions, ladder_to_positions):
+    """
+    Finite Markov Process of Snakes and Ladders
+    """
+    def __init__(self, snake_positions: list, snake_to_positions: list,
+                 ladder_positions: list, ladder_to_positions: list):
         self.snake_positions = snake_positions
         self.snake_to_positions = snake_to_positions
         self.ladder_positions = ladder_positions
@@ -22,6 +25,9 @@ class SnakesAndLaddersMPFinite(FiniteMarkovProcess[PlayerState]):
         super().__init__(self.get_transition_map())
 
     def get_transition_map(self) -> Transition[PlayerState]:
+        """
+        return transition map
+        """
         d: Dict[PlayerState, Optional[Categorical[PlayerState]]] = {}
         for i in range(101):
             states_prob_map: Mapping[PlayerState, float] = {}
@@ -43,7 +49,9 @@ class SnakesAndLaddersMPFinite(FiniteMarkovProcess[PlayerState]):
 
 
 class SnakesAndLaddersMRPFinite(FiniteMarkovRewardProcess[PlayerState]):
-
+    """
+        Finite Markov Reward Process of Snakes and Ladders
+        """
     def __init__(self, snake_positions, snake_to_positions, ladder_positions, ladder_to_positions):
         self.snake_positions = snake_positions
         self.snake_to_positions = snake_to_positions
@@ -52,6 +60,9 @@ class SnakesAndLaddersMRPFinite(FiniteMarkovRewardProcess[PlayerState]):
         super().__init__(self.get_transition_reward_map())
 
     def get_transition_reward_map(self) -> RewardTransition[PlayerState]:
+        """
+        return transition reward map
+        """
         d: Dict[PlayerState, Optional[Categorical[Tuple[PlayerState, int]]]] = {}
         for i in range(101):
             states_prob_map: Mapping[Tuple[PlayerState, int], float] = {}
@@ -74,15 +85,18 @@ class SnakesAndLaddersMRPFinite(FiniteMarkovRewardProcess[PlayerState]):
 
 
 if __name__ == '__main__':
-
+    # construct the game
     snake_to_positions = [1, 6, 8, 14, 17, 34, 37, 50, 42, 54, 63]
     snake_positions = [38, 31, 49, 65, 53, 70, 76, 88, 94, 98, 82]
     ladder_to_positions = [35, 39, 41, 48, 51, 57, 74, 83, 85, 90, 92]
     ladder_positions = [28, 3, 20, 7, 12, 25, 45, 77, 60, 67, 69]
     sl_mp = SnakesAndLaddersMPFinite(snake_positions, snake_to_positions, ladder_positions, ladder_to_positions)
     sl_mrp = SnakesAndLaddersMRPFinite(snake_positions, snake_to_positions, ladder_positions, ladder_to_positions)
-    print(sl_mrp.display_value_function(1))
 
+    print('Expected number of dices calculated by value function:',
+          sl_mrp.get_value_function_vec(1)[0])
+
+    # simulate
     transition_map = sl_mp.get_transition_map()
     traces_steps = []
     count = 0
@@ -93,14 +107,19 @@ if __name__ == '__main__':
             step += 1
             if pos == PlayerState(100):
                 traces_steps.append(step)
-        if count == 50000:
+        if count == 100000:
             break
+
+    # plot the probability distribution
+    # x = list(set(traces_steps))
+    # y = [traces_steps.count(step)/50000 for step in x]
     plt.hist(traces_steps, bins=20)
     plt.show()
 
+    # Compare the expected number of dices calculated by simulation
     mean = 0
     for step in set(traces_steps):
-        step_freq = traces_steps.count(step)/50000
+        step_freq = traces_steps.count(step)/100000
         mean += step*step_freq
-    print('simulated expected number of dices:', mean)
+    print('Simulated expected number of dices:', mean)
 
