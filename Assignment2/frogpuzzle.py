ae166a0d1d03835aa51c071b43250f44443c2610
guyiyun1 <yiyun.gu@mail.utoronto.ca>
2021-01-27 13:43:07 +0800

    A2/Frog Puzzle Markov Process

--- Assignment2/frogpuzzle.py
+++ Assignment2/frogpuzzle.py
@@ -5,19 +5,24 @@ import matplotlib.pyplot as plt
 
 @dataclass(frozen=True)
 class FrogState:
+    """
+    Frog state
+    """
     position: int
-
-    def frog_position(self) -> int:
-        return self.position
 
 
 class FrogPuzzleMPFinite(FiniteMarkovProcess[FrogState]):
-
+    """
+    Finite Markov Process of the Frog Puzzle
+    """
     def __init__(self, num_leaves):
         self.num_leaves = num_leaves
         super().__init__(self.get_transition_map())
 
     def get_transition_map(self) -> Transition[FrogState]:
+        """
+        return transition map
+        """
         d: Dict[FrogState, Optional[Categorical[FrogState]]] = {}
         for i in range(self.num_leaves):
             states_prob_map: Mapping[FrogState, float] = {}
@@ -32,6 +37,8 @@ if __name__ == '__main__':
     fp_mp = FrogPuzzleMPFinite(10)
     print(fp_mp)
     transition_map = fp_mp.get_transition_map()
+
+    # calculate the expected jumps by simulation
     traces_steps = []
     count = 0
     for trace in fp_mp.traces(transition_map[FrogState(0)]):
@@ -41,14 +48,14 @@ if __name__ == '__main__':
             step += 1
             if pos == FrogState(10):
                 traces_steps.append(step)
-        if count == 10000:
+        if count == 50000:
             break
-    plt.hist(traces_steps, bins=20)
-    plt.show()
+    # plt.hist(traces_steps, bins=20)
+    # plt.show()
 
     mean = 0
     for step in set(traces_steps):
-        step_freq = traces_steps.count(step) / 10000
+        step_freq = traces_steps.count(step) / 50000
         mean += step * step_freq
-    print(mean)
+    print('Expected jumps:', mean)
 
