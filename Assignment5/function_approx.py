be6d1198d05a62c0792e133c7fb30555a2a7a46d
guyiyun1 <yiyun.gu@mail.utoronto.ca>
2021-02-20 22:14:54 +0800

    Univariate b-spline

--- Assignment5/function_approx.py
+++ Assignment5/function_approx.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import UnivariateSpline
 import numpy as np
 
-
 x = np.linspace(-3, 3, 50)
 y = np.exp(x) + 0.1*np.random.randn(50)
 plt.plot(x, y, 'ro')
