30cdeef42d8068ad6a00055e8da6c7cfc45adb8e
guyiyun1 <yiyun.gu@mail.utoronto.ca>
2021-03-06 23:47:42 +0800

    TD lambda

---
+++ Assignment12/compare.py
@@ -0,0 +1,60 @@
+from rl.chapter2.simple_inventory_mrp import SimpleInventoryMRPFinite, InventoryState
+from rl.markov_process import FiniteMarkovProcess
+from rl.distribution import Categorical
+from Assignment11.td_scratch import td_prediction
+from dataclasses import dataclass, replace, field
+from Assignment11.monte_carlo_scratch import mc_prediction
+from Assignment12.td_nbootstrap import td_nbootstrap_tabular_prediction
+from Assignment12.td_lambda import td_lambda_tabular_prediction
+import matplotlib.pyplot as plt
+
+user_capacity = 2
+user_poisson_lambda = 1.0
+user_holding_cost = 1.0
+user_stockout_cost = 10.0
+
+user_gamma = 0.9
+
+si_mrp = SimpleInventoryMRPFinite(
+    capacity=user_capacity,
+    poisson_lambda=user_poisson_lambda,
+    holding_cost=user_holding_cost,
+    stockout_cost=user_stockout_cost
+)
+
+print("-----MRP Value Function-----:\n")
+print(si_mrp.get_value_function_vec(user_gamma))
+
+
+# create Iterable[TransitionStep[S]]
+non_terminal_states = si_mrp.non_terminal_states
+start_distribution = {state: 1/len(non_terminal_states) for state in non_terminal_states}
+transitions = si_mrp.simulate_reward(Categorical(start_distribution))
+
+def count_to_weight_func(n: int):
+    return 1/n
+
+print("-----TD Value Function-----:\n")
+td_pred = td_prediction(transitions, count_to_weight_func, user_gamma)
+print(td_pred.evaluate(non_terminal_states))
+
+print("-----MC Value Function-----:\n")
+mc_pred = mc_prediction(transitions, count_to_weight_func, user_gamma)
+print(mc_pred.evaluate(non_terminal_states))
+
+print("-----TD NBOOTSTRAP Value Function-----:\n")
+td_boot_pred = td_nbootstrap_tabular_prediction(transitions, count_to_weight_func, user_gamma, 10)
+print(td_boot_pred.evaluate(non_terminal_states))
+
+print("-----TD Lambda Value Function-----:\n")
+converge_iters = []
+lambdas = [0.75, 1]  # 0, 0.25, 0.5,
+for lambd in lambdas:
+    td_lambda_pred, converge = td_lambda_tabular_prediction(transitions, count_to_weight_func, user_gamma, 0.5)
+    print(td_lambda_pred.evaluate(non_terminal_states), converge)
+    converge_iters.append(converge)
+
+plt.plot(lambdas, converge_iters, 'o')
+plt.xlabel('Lambda')
+plt.ylabel('Converge Iterations')
+plt.show()
---
+++ Assignment12/td_lambda.py
@@ -0,0 +1,65 @@
+from typing import Callable, Iterable, Iterator, TypeVar, Tuple, Dict
+
+from rl.function_approx import FunctionApprox, Tabular
+import rl.markov_process as mp
+import rl.markov_decision_process as mdp
+import rl.iterate as iterate
+from rl.returns import returns
+import operator
+
+S = TypeVar('S')
+
+def td_lambda_tabular_prediction(
+        transitions: Iterable[mp.TransitionStep[S]],
+        count_to_weight_func: Callable[[int], float],
+        gamma: float,
+        lambd: float,
+        max_steps: int = 4000,
+        tolerance: float = 1e-200
+) -> Tuple[Tabular[S], int]:
+    """
+    Similar to TD Scratch except replacing use G_{t,n} for updates
+    """
+    values_map: Dict[S, float] = {}
+    counts_map: Dict[S, int] = {}
+    trace = []
+    count = 0
+    diff = {}  # dict: state and its value error
+    for transition in transitions:
+        count += 1
+        trace.append(transition)
+        if count > max_steps:
+            break
+
+    # get corresponding return
+    transitions_returns = returns(trace, gamma, tolerance)
+    trace_returns = [return_ for return_ in transitions_returns]
+
+    for i in range(max_steps):
+        transition = trace[i]
+        state = transition.state
+        if state not in diff:
+            diff[state] = 100
+        counts_map[state] = counts_map.get(state, 0) + 1
+        weight: float = count_to_weight_func(counts_map.get(state, 0))
+        if transition.next_state not in values_map:
+            values_map[transition.next_state] = -30
+        y = lambd**(max_steps-i-1) * trace_returns[i].return_
+        if lambd == 0:
+            y = 0
+        for n in range(1, max_steps-i):
+            g_tn = 0
+            for j in range(i, i+n):
+                next_transition = trace[j]
+                g_tn += gamma**(j-i) * next_transition.reward
+                if j == i+n-1:
+                    g_tn += gamma**n * values_map.get(next_transition.next_state, 0)
+            y += (1-lambd) * lambd**(n-1) * g_tn
+        diff[state] = min(abs(y - values_map.get(state, 0.)), diff[state])
+        values_map[state] = weight * y + (1 - weight) * values_map.get(state, 0.)
+        # print(y, values_map[state])
+        count += 1
+        if diff[max(diff.items(), key=operator.itemgetter(1))[0]] < 0.1:
+            break
+    print(diff[max(diff.items(), key=operator.itemgetter(1))[0]])
+    return Tabular(values_map, counts_map, count_to_weight_func), i
