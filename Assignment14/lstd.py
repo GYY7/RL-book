25e8d111b89380775e02f77ced10070cb985df52
guyiyun1 <yiyun.gu@mail.utoronto.ca>
2021-03-08 15:35:58 +0800

    LSTD

---
+++ Assignment14/lstd.py
@@ -0,0 +1,24 @@
+from typing import Callable, Iterable, Iterator, TypeVar, Tuple, Dict, List
+import rl.markov_process as mp
+import numpy as np
+
+S = TypeVar('S')
+
+
+def lstd(transitions: Iterable[mp.TransitionStep[S]],
+         feature_map: Dict[S, List[float]],
+         m: int,
+         gamma: float) -> Iterable[List[float]]:
+    """
+    update A and b to get w*= inverse(A)b
+    feature_map:  key: state, value: phi(s_i) is a vector of dimension m
+    """
+    # initialize A, b
+    A = np.random.rand(m, m)
+    b = np.zeros((m, 1))
+    for transition in transitions:
+        feature_state = np.array(feature_map[transition.state])
+        feature_next_state = np.array(feature_map[transition.next_state])
+        A += feature_state @ (feature_state - gamma*feature_next_state).T
+        b += feature_state * transition.reward
+        yield np.linalg.inv(A) @ b
